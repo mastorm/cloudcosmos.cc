a6bb6db2e9db5470efaf9d46545d194ef959c488
Mathias Storm <mathias@storm.lc>
2023-10-03 17:21:09 +0200

    enable ssr and configure astro-sst export

--- packages/blog/astro.config.mjs
+++ packages/blog/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
-
+import aws from "astro-sst/lambda";
 // https://astro.build/config
 export default defineConfig({
-    outDir: 'dist/client'
+    outDir: 'dist/client',
+    adapter: aws(),
+    output: "server",
 });
