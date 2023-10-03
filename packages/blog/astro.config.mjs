0aaeb1f5e996e04380d2b6178646f58d4d3f4b18
Mathias Storm <mathias@storm.lc>
2023-10-03 17:22:49 +0200

    remove bogus build  output

--- packages/blog/astro.config.mjs
+++ packages/blog/astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import aws from "astro-sst/lambda";
 // https://astro.build/config
 export default defineConfig({
-    outDir: 'dist/client',
     adapter: aws(),
     output: "server",
 });
