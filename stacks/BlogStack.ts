e4977f6b666862ad149d10d0193e1789a34ab911
Mathias Storm <mathias@storm.lc>
2023-10-03 16:52:00 +0200

    add astro site

--- stacks/BlogStack.ts
+++ stacks/BlogStack.ts
@@ -1,5 +1,11 @@
-import { StackContext, Api, EventBus } from "sst/constructs";
+import { StackContext, AstroSite } from "sst/constructs";
 
 export function Blog({ stack }: StackContext) {
-  
+
+  const blog = new AstroSite(stack, "blog", {
+    path: 'packages/blog'
+  })
+  stack.addOutputs({
+    URL: blog.url
+  })
 }
