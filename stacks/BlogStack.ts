09d58e5cebcf5fefce6a78042188b3ca24b5f76a
Mathias Storm <mathias@storm.lc>
2023-10-04 18:59:26 +0200

    configure route53 domain

--- stacks/BlogStack.ts
+++ stacks/BlogStack.ts
@@ -3,7 +3,8 @@ import { StackContext, AstroSite } from "sst/constructs";
 export function Blog({ stack }: StackContext) {
 
   const blog = new AstroSite(stack, "blog", {
-    path: 'packages/blog'
+    path: 'packages/blog',
+    customDomain: 'cloudcosmos.cc'
   })
   stack.addOutputs({
     URL: blog.url,
